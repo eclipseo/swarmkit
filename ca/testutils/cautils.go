240b7320583abd8bbafed1d5be28baf62f97756a
Robert-André Mauchin <zebob.m@gmail.com>
2020-01-30 19:49:33 +0100

    Convert BasicKeyRequest to KeyRequest to use cloudflare/cfssl 1.4.1
    Signed-off-by: Robert-André Mauchin <zebob.m@gmail.com>

--- ca/testutils/cautils.go
+++ ca/testutils/cautils.go
@@ -345,7 +345,7 @@ func createNode(s *store.MemoryStore, nodeID, role string, csr, cert []byte) err
 
 func genSecurityConfig(s *store.MemoryStore, rootCA ca.RootCA, krw *ca.KeyReadWriter, role, org, tmpDir string, nonSigningRoot bool) (*ca.SecurityConfig, func() error, error) {
 	req := &cfcsr.CertificateRequest{
-		KeyRequest: cfcsr.NewBasicKeyRequest(),
+		KeyRequest: cfcsr.NewKeyRequest(),
 	}
 
 	csr, key, err := cfcsr.ParseRequest(req)
@@ -448,7 +448,7 @@ func CreateRootCertAndKey(rootCN string) ([]byte, []byte, error) {
 	// Create a simple CSR for the CA using the default CA validator and policy
 	req := cfcsr.CertificateRequest{
 		CN:         rootCN,
-		KeyRequest: cfcsr.NewBasicKeyRequest(),
+		KeyRequest: cfcsr.NewKeyRequest(),
 		CA:         &cfcsr.CAConfig{Expiry: ca.RootCAExpiration},
 	}
 
@@ -490,7 +490,7 @@ func ReDateCert(t *testing.T, cert, signerCert, signerKey []byte, notBefore, not
 func CreateCertFromSigner(rootCN string, priv crypto.Signer) ([]byte, error) {
 	req := cfcsr.CertificateRequest{
 		CN:         rootCN,
-		KeyRequest: &cfcsr.BasicKeyRequest{A: ca.RootKeyAlgo, S: ca.RootKeySize},
+		KeyRequest: &cfcsr.KeyRequest{A: ca.RootKeyAlgo, S: ca.RootKeySize},
 		CA:         &cfcsr.CAConfig{Expiry: ca.RootCAExpiration},
 	}
 	cert, _, err := initca.NewFromSigner(&req, priv)
